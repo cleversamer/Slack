647c74ca4fb33577d51c74ce071a619778ad8e9f
Samer A <thedev.samer@gmail.com>
2022-11-09 09:46:56 +0200

    code refactoring

--- public/joinNs.js
+++ public/joinNs.js
@@ -1,56 +1,58 @@
-function joinNs(endpoint){
-    if(nsSocket){
-        // check to see if nsSocket is actually a socket
-        nsSocket.close();
-        // remove the eventListener before it's added again
-        document.querySelector('#user-input').removeEventListener('submit',formSubmission)
-    }
-    nsSocket = io(`http://localhost:9000${endpoint}`)
-    nsSocket.on('nsRoomLoad',(nsRooms)=>{
-        // console.log(nsRooms)
-        let roomList = document.querySelector('.room-list');
-        roomList.innerHTML = "";
-        nsRooms.forEach((room)=>{
-            let glyph;
-            if(room.privateRoom){
-                glyph = 'lock'
-            }else{
-                glyph = 'globe'
-            }
-            roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`
-        })
-        // add click listener to each room
-        let roomNodes = document.getElementsByClassName('room');
-        Array.from(roomNodes).forEach((elem)=>{
-            elem.addEventListener('click',(e)=>{
-                // console.log("Somone clicked on ",e.target.innerText);
-                joinRoom(e.target.innerText)
-            })
-        })
-        // add room automatically... first time here
-        const topRoom = document.querySelector('.room')
-        const topRoomName = topRoom.innerText;
-        // console.log(topRoomName);
-        joinRoom(topRoomName)
-        
-    })    
-    nsSocket.on('messageToClients',(msg)=>{
-        console.log(msg)
-        const newMsg = buildHTML(msg);
-        document.querySelector('#messages').innerHTML += newMsg
-    })
-    document.querySelector('.message-form').addEventListener('submit',formSubmission)
+function joinNs(endpoint) {
+  if (nsSocket) {
+    // check to see if nsSocket is actually a socket
+    nsSocket.close();
+    // remove the eventListener before it's added again
+    document
+      .querySelector("#user-input")
+      .removeEventListener("submit", formSubmission);
+  }
+
+  // Connect to the namespace
+  nsSocket = io(`http://localhost:9000${endpoint}`);
+
+  nsSocket.on("nsRoomLoad", (nsRooms) => {
+    let roomList = document.querySelector(".room-list");
+    roomList.innerHTML = "";
+    nsRooms.forEach((room) => {
+      let glyph = room.privateRoom ? "lock" : "globe";
+      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
+    });
+
+    // add click listener to each room
+    let roomNodes = document.getElementsByClassName("room");
+    Array.from(roomNodes).forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        // console.log("Somone clicked on ",e.target.innerText);
+        joinRoom(e.target.innerText);
+      });
+    });
+
+    // add room automatically... first time here
+    const topRoom = document.querySelector(".room");
+    const topRoomName = topRoom.innerText;
+    joinRoom(topRoomName);
+  });
+
+  nsSocket.on("messageToClients", (msg) => {
+    const newMsg = buildHTML(msg);
+    document.querySelector("#messages").innerHTML += newMsg;
+  });
+
+  document
+    .querySelector(".message-form")
+    .addEventListener("submit", formSubmission);
 }
 
-function formSubmission(event){
-    event.preventDefault();
-    const newMessage = document.querySelector('#user-message').value;
-    nsSocket.emit('newMessageToServer',{text: newMessage})
+function formSubmission(event) {
+  event.preventDefault();
+  const newMessage = document.querySelector("#user-message").value;
+  nsSocket.emit("newMessageToServer", { text: newMessage });
 }
 
-function buildHTML(msg){
-    const convertedDate = new Date(msg.time).toLocaleString();
-    const newHTML = `
+function buildHTML(msg) {
+  const convertedDate = new Date(msg.time).toLocaleString();
+  const newHTML = `
     <li>
         <div class="user-image">
             <img src="${msg.avatar}" />
@@ -60,6 +62,6 @@ function buildHTML(msg){
             <div class="message-text">${msg.text}</div>
         </div>
     </li>    
-    `
-    return newHTML;
+    `;
+  return newHTML;
 }
--- slack.js
+++ slack.js
@@ -3,23 +3,21 @@ const app = express();
 const socketio = require("socket.io");
 
 let namespaces = require("./data/namespaces");
-// console.log(namespaces[0]);
+
 app.use(express.static(__dirname + "/public"));
+
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
 // io.on = io.of('/').on = io.sockets.on
 // io.emit = io.of('/').emit = io.sockets.emit
 io.on("connection", (socket) => {
-  // console.log(socket.handshake)
   // build an array to send back with the img and endpoing for each NS
-  let nsData = namespaces.map((ns) => {
-    return {
-      img: ns.img,
-      endpoint: ns.endpoint,
-    };
-  });
-  // console.log(nsData)
+  let nsData = namespaces.map((ns) => ({
+    img: ns.img,
+    endpoint: ns.endpoint,
+  }));
+
   // sned the nsData back to the client. We need to use socket, NOT io, because we want it to
   // go to just this client.
   socket.emit("nsList", nsData);
@@ -27,32 +25,28 @@ io.on("connection", (socket) => {
 
 // loop through each namespace and listen for a connection
 namespaces.forEach((namespace) => {
-  // console.log(namespace)
-  // const thisNs = io.of(namespace.endpoint)
   io.of(namespace.endpoint).on("connection", (nsSocket) => {
-    console.log(nsSocket.handshake);
     const username = nsSocket.handshake.query.username;
-    // console.log(`${nsSocket.id} has join ${namespace.endpoint}`)
+
     // a socket has connected to one of our chatgroup namespaces.
     // send that ns gorup info back
     nsSocket.emit("nsRoomLoad", namespace.rooms);
+
     nsSocket.on("joinRoom", (roomToJoin, numberOfUsersCallback) => {
       // deal with history... once we have it
-      console.log(nsSocket.rooms);
       const roomToLeave = Object.keys(nsSocket.rooms)[1];
       nsSocket.leave(roomToLeave);
+
       updateUsersInRoom(namespace, roomToLeave);
       nsSocket.join(roomToJoin);
-      // io.of('/wiki').in(roomToJoin).clients((error, clients)=>{
-      //     console.log(clients.length)
-      //     numberOfUsersCallback(clients.length);
-      // })
-      const nsRoom = namespace.rooms.find((room) => {
-        return room.roomTitle === roomToJoin;
-      });
+
+      const nsRoom = namespace.rooms.find(
+        (room) => room.roomTitle === roomToJoin
+      );
       nsSocket.emit("historyCatchUp", nsRoom.history);
       updateUsersInRoom(namespace, roomToJoin);
     });
+
     nsSocket.on("newMessageToServer", (msg) => {
       const fullMsg = {
         text: msg.text,
@@ -60,20 +54,18 @@ namespaces.forEach((namespace) => {
         username: username,
         avatar: "https://via.placeholder.com/30",
       };
-      // console.log(fullMsg)
+
       // Send this message to ALL the sockets that are in the room that THIS socket is in.
       // how can we find out what rooms THIS socket is in?
-      // console.log(nsSocket.rooms)
       // the user will be in the 2nd room in the object list
       // this is because the socket ALWAYS joins its own room on connection
       // get the keys
       const roomTitle = Object.keys(nsSocket.rooms)[1];
+
       // we need to find the Room object for this room
       const nsRoom = namespace.rooms.find((room) => {
         return room.roomTitle === roomTitle;
       });
-      // console.log("The room object that we made that matches this NS room is...")
-      // console.log(nsRoom)
       nsRoom.addMessage(fullMsg);
       io.of(namespace.endpoint).to(roomTitle).emit("messageToClients", fullMsg);
     });
@@ -85,9 +77,10 @@ function updateUsersInRoom(namespace, roomToJoin) {
   io.of(namespace.endpoint)
     .in(roomToJoin)
     .clients((error, clients) => {
-      // console.log(`There are ${clients.length} in this room`);
-      io.of(namespace.endpoint)
-        .in(roomToJoin)
-        .emit("updateMembers", clients.length);
+      if (!error) {
+        io.of(namespace.endpoint)
+          .in(roomToJoin)
+          .emit("updateMembers", clients.length);
+      }
     });
 }
